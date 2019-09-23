149b653daf9a1bd5e750c388fab8c3a23ce45c3d
josie <josieopjam@gmail.com>
2019-09-23 16:20:43 +0100

    fizzbuzz

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,3 +1,13 @@
 def fizzbuzz(number)
-"fizz"
+(1..100).each do |number|
+  if (number % 3).zero? && (number % 5).zero?
+    puts "FizzBuzz"
+  elsif (number % 3).zero?
+    puts "Fizz"
+  elsif (number % 5).zero?
+    puts "Buzz"
+  else
+    puts number
+  end
+end
 end
